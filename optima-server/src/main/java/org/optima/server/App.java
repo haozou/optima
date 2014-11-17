ba53b0d13327572692b7505a444f8070c58b4fc7
Hao Zou <hzou@pivotal.io>
2014-11-17 13:26:47 -0800

    hive service added

---
+++ optima-server/src/main/java/org/optima/server/App.java
@@ -0,0 +1,28 @@
+package org.optima.server;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import org.optima.server.services.HiveService;
+
+/**
+ * Example code
+ *
+ */
+public class App {
+	public static void main(String[] args) {
+		try {
+			HiveService hive = new HiveService("localhost", "default", "hzou");
+			hive.createTable("poo", true);
+			ResultSet res = hive.showTables();
+			while (res.next()) {
+				System.out.println(res.getString(1));
+			}
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+
+	}
+}
